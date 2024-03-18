7209c651f6353c9ca1e2fd791d4bc953d8c41b02
Wyatt <wyattbertorelli@BitsyBook-Air.local>
2024-03-17 17:26:43 -0700

    Reference manually created GitHub access token for codepipeline access

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -9,7 +9,9 @@ export class PipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'Pipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('Wbertore/WbertoreCDK', 'main'),
+        input: CodePipelineSource.gitHub('Wbertore/WbertoreCDK', 'main', {
+          authentication: cdk.SecretValue.secretsManager('github-access-token-codepipeline'),
+        }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
