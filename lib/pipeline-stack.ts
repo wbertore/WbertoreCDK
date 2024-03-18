32d1431c92c657e746f51973b170f60fa58768eb
Wyatt <wyattbertorelli@BitsyBook-Air.local>
2024-03-17 17:35:04 -0700

    Fix typo in username

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -9,7 +9,7 @@ export class PipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'Pipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('Wbertore/WbertoreCDK', 'main', {
+        input: CodePipelineSource.gitHub('wbertore/WbertoreCDK', 'main', {
           authentication: cdk.SecretValue.secretsManager('github-access-token-codepipeline'),
         }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
