c4f5deb943d3b48a511cb2c3988033a76782ae87
Wyatt <wyattbertorelli@BitsyBook-Air.local>
2024-04-07 15:04:24 -0600

    Use AL2023 for Lambda runtime for newer GLIBC

--- lib/website-stack.ts
+++ lib/website-stack.ts
@@ -18,7 +18,10 @@ export class WebsiteStack extends cdk.Stack {
         const rustArtifactKey = new cdk.CfnParameter(this, RUST_ARTIFACT_S3_KEY_PARAM_NAME);
         const websiteBackend = new Function(this, "website-backend", {
             code: Code.fromBucket(props.rustArtifactBucket, rustArtifactKey.valueAsString),
-            runtime: Runtime.PROVIDED_AL2,
+            // As of 2024-04-07, the rust bootstrap requires GLIBC_2.28. AL2 has too old of a version.
+            // Try using AL2023:
+            // https://docs.aws.amazon.com/linux/al2023/ug/compare-with-al2.html#glibc-gcc-and-binutils
+            runtime: Runtime.PROVIDED_AL2023,
             architecture: Architecture.ARM_64,
             timeout: cdk.Duration.seconds(60),
             // HUH???? apparently other people are doing this:
