b1c1cbaf06447b0fb326ba01c5ec7314232f40be
Wyatt <me@wbertore.dev>
2026-03-23 13:58:30 -0600

    Stub out s3 parameter for rust - deployment failure from our hack
    We eventually need this when we add an async lambda

--- lib/expense-stack.ts
+++ lib/expense-stack.ts
@@ -1,12 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Bucket, LifecycleRule } from 'aws-cdk-lib/aws-s3';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { RUST_ARTIFACT_S3_KEY_PARAM_NAME } from './common';
 
 export const RECEIPT_UPLOADS_BUCKET_EXPORT = 'ReceiptUploadsBucketName';
 
 export class ExpenseStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        // Declared so the pipeline parameter override applies when a Lambda is added to this stack
+        new cdk.CfnParameter(this, RUST_ARTIFACT_S3_KEY_PARAM_NAME);
 
         const receiptUploadsBucket = new Bucket(this, 'receipt-uploads', {
             removalPolicy: cdk.RemovalPolicy.DESTROY,
